61ad6130ee504366d72a8b2dfedfb6b3c584b872
Pablo Ventura <pablogventura@gmail.com>
2025-04-10 16:13:15 -0300

    Actualizar script de instalación de OMNeT++ para incluir bibliotecas de Python y entorno virtual

--- install_omnet.sh
+++ install_omnet.sh
@@ -7,11 +7,10 @@ sudo apt-get update
 sudo apt-get -y install build-essential clang lld gdb bison flex perl \
 python3 python3-pip qtbase5-dev qtchooser qt5-qmake qtbase5-dev-tools \
 libqt5opengl5-dev libxml2-dev zlib1g-dev doxygen graphviz \
-libwebkit2gtk-4.0-37 xdg-utils
+xdg-utils
+sudo apt-get -y install libwebkit2gtk-4.0-37
+sudo apt-get -y install python3-numpy python3-scipy python3-matplotlib python3-pandas python3-seaborn
 
-# Actualizar las bibliotecas de Python
-python3 -m pip install --user --upgrade numpy pandas matplotlib scipy \
-seaborn posix_ipc
 
 # Instalar bibliotecas para el desarrollo de MPI
 sudo apt-get -y install mpi-default-dev
@@ -24,6 +23,15 @@ wget -c https://github.com/omnetpp/omnetpp/releases/download/omnetpp-6.0.1/omnet
 
 # Descomprimir el archivo descargado
 tar xzf omnetpp-6.0.1-linux-x86_64.tgz
+
+python3 -m venv omnetpp-6.0.1/venv
+
+# Activar el entorno
+source omnetpp-6.0.1/venv/bin/activate
+
+# Instalar las dependencias de Python
+pip install --upgrade pip
+pip install numpy pandas matplotlib scipy seaborn posix_ipc
 
 # Navegar al directorio de OMNeT++
 cd omnetpp-6.0.1
@@ -45,3 +53,4 @@ make install-shortcuts
 
 # Permitir el acceso a PTRACE para depuración
 sudo sed -i 's/kernel.yama.ptrace_scope = 1/kernel.yama.ptrace_scope = 0/' /etc/sysctl.d/10-ptrace.conf
+
